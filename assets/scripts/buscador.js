49833b521bb8f843bf2e0e040cc64a7c49089f44
Emmanuel-Garnica <56373024+Emmanuel-Garnica@users.noreply.github.com>
2020-03-19 21:29:03 -0500

    En esta actualización se logra hacer funcionar por completo el fetch de la búsqueda para que aparezcan los gifs en sus respectivos contenedores, además de cmabiar el título de la seción de los resultados por el string escrito en el input.

--- assets/scripts/buscador.js
+++ assets/scripts/buscador.js
@@ -3,11 +3,25 @@ function buscarGif() {
     const apiKey = "nlzm1uqvJd1LB9FZMds6OIhmeoZm6AIh";
     let found = fetch('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + apiKey)
         .then((res) => res.json())
-        .then((data) => data.data[0].images.downsized_medium.url)
-        .then((url) => document.getElementById("prueba").setAttribute("src", url))
+        .then((data) => data.data)
+        .then((array) => array.map((images) => images.images.preview_webp.url))
+        .then((urls) => {
+            let i = 0;
+            urls.forEach((e) => {
+                document.querySelectorAll(".gif_result")[i].setAttribute("src", e)
+                i++;
+            })
+        })
+    document.querySelector(".title_trending").innerHTML = search + ":";
+    // .then((data) => data.data[0].images.downsized_medium.url)
+    // .then((url) => document.getElementById("prueba").setAttribute("src", url))
 }
 
 
 // .then((array) => array.forEach((gif) => {
 //     console.log(gif.url)
 // }))
+
+// window.onload = function trendingGifs() {
+
+// }
