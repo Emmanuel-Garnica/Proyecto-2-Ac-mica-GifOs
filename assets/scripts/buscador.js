61cb87f3d7a6e6205e8eb1e453aaddbbcb74548d
Emmanuel-Garnica <56373024+Emmanuel-Garnica@users.noreply.github.com>
2020-03-20 20:52:45 -0500

    En esta actualización se soluciona el problema del fetch del buscador, ahora aparecen las tendencias de los gifs por defecto en cada contenedor y también al buscar los cambia el contenido de los mismos, por último también se extrae de la API el titulo de cada gif para que aparezca al hacer hover sobre cada contenedor.

--- assets/scripts/buscador.js
+++ assets/scripts/buscador.js
@@ -1,7 +1,7 @@
 function buscarGif() {
     let search = document.querySelector("input").value;
     const apiKey = "nlzm1uqvJd1LB9FZMds6OIhmeoZm6AIh";
-    let found = fetch('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + apiKey)
+    let found = fetch('http://api.giphy.com/v1/gifs/search?limit=20&q=' + search + '&api_key=' + apiKey)
         .then((res) => res.json())
         .then((data) => data.data)
         .then((array) => array.map((images) => images.images.preview_webp.url))
@@ -12,15 +12,21 @@ function buscarGif() {
                 i++;
             })
         })
+
     document.querySelector(".title_trending").innerHTML = search + ":";
-    // .then((data) => data.data[0].images.downsized_medium.url)
-    // .then((url) => document.getElementById("prueba").setAttribute("src", url))
+
+    fetch('http://api.giphy.com/v1/gifs/search?limit=20&q=' + search + '&api_key=' + apiKey)
+        .then((response) => response.json())
+        .then((data) => data.data)
+        .then((array) => array.map((title) => title.title))
+        .then((text) => {
+            let i = 0;
+            text.forEach((e) => {
+                document.querySelectorAll(".gif_result_title")[i].innerText = e
+                i++;
+            })
+        })
 }
-
-
-// .then((array) => array.forEach((gif) => {
-//     console.log(gif.url)
-// }))
 
 // window.onload = function trendingGifs() {
 
--- assets/scripts/tendencias.js
+++ assets/scripts/tendencias.js
@@ -1,3 +1,27 @@
-const apiKey = "nlzm1uqvJd1LB9FZMds6OIhmeoZm6AIh";
-let suggestions = fetch('http://api.giphy.com/v1/gifs/trending?' + 'api_key=' + apiKey)
-    .then((response) => response.json())
+function suggestionsGif() {
+    const apiKey = "nlzm1uqvJd1LB9FZMds6OIhmeoZm6AIh";
+    let suggestions = fetch('http://api.giphy.com/v1/gifs/trending?limit=20&api_key=' + apiKey)
+        .then((response) => response.json())
+        .then((data) => data.data)
+        .then((array) => array.map((images) => images.images.preview_webp.url))
+        .then((urls) => {
+            let i = 0;
+            urls.forEach((e) => {
+                document.querySelectorAll(".gif_result")[i].setAttribute("src", e)
+                i++;
+            })
+        })
+
+    fetch('http://api.giphy.com/v1/gifs/trending?limit=20&api_key=' + apiKey)
+        .then((response) => response.json())
+        .then((data) => data.data)
+        .then((array) => array.map((title) => title.title))
+        .then((text) => {
+            let i = 0;
+            text.forEach((e) => {
+                document.querySelectorAll(".gif_result_title")[i].innerText = e
+                i++;
+            })
+        })
+}
+suggestionsGif();
