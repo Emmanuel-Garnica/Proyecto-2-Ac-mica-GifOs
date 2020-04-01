99f09834683d6de5f7c19c3e11f84531205a486c
Emmanuel-Garnica <56373024+Emmanuel-Garnica@users.noreply.github.com>
2020-04-01 18:01:35 -0500

    GRABANDO GIF - Se logra pedir permiso de uso de la cámara al usuario y reflejarlo en pantalla con su correspondiente tag.

--- assets/scripts/subir_gifos.js
+++ assets/scripts/subir_gifos.js
@@ -2,3 +2,32 @@ function startButton() {
     document.querySelector(".recordGif").style.display = "block";
     document.querySelector(".createGif").style.display = "none";
 }
+
+function allowRecord() {
+    navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+            height: 440,
+            width: 838
+        }
+    })
+
+    .then(function(recordGif) {
+
+        let video_Container = document.getElementsByTagName("video")[0];
+
+        video_Container.srcObject = recordGif;
+        video_Container.play();
+    })
+}
+
+function record() {
+    startButton();
+    allowRecord();
+}
+
+// let recordGifDisplay = document.querySelector(".createGif").style.display;
+
+// if (recordGifDisplay === "block") {
+//     allowRecord();
+// }
