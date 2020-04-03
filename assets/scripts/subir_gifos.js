8e171530b5975fc30d2a3c9a4022bab9ebd10cf1
Emmanuel-Garnica <56373024+Emmanuel-Garnica@users.noreply.github.com>
2020-04-02 21:51:26 -0500

    GRABANDO GIF - Se agrega funcionalidad a más botones, se agregan ID's y clases, se maqueta un nuevo botón e imagen y se arregla legibilidad de código.

--- assets/scripts/subir_gifos.js
+++ assets/scripts/subir_gifos.js
@@ -14,11 +14,16 @@ function allowRecord() {
 
     .then(function(recordGif) {
 
-        let video_Container = document.getElementsByTagName("video")[0];
+            let video_Container = document.querySelector("video");
 
-        video_Container.srcObject = recordGif;
-        video_Container.play();
-    })
+            video_Container.srcObject = recordGif;
+            video_Container.play();
+        })
+        .catch(function(error) {
+
+            alert("Es necesario permitir el uso de la cámara para continuar - " + error)
+
+        })
 }
 
 function record() {
@@ -26,8 +31,53 @@ function record() {
     allowRecord();
 }
 
-// let recordGifDisplay = document.querySelector(".createGif").style.display;
+function startRecording() {
 
-// if (recordGifDisplay === "block") {
-//     allowRecord();
-// }
+    let captureButton = document.querySelector(".capture_button");
+    let cameraImg = document.getElementById("cameraImg");
+    let finishCaptureButton = document.querySelector(".finish_capture_button");
+    let recordImg = document.getElementById("recordImg");
+
+    captureButton.style.display = "none";
+    cameraImg.style.display = "none";
+    finishCaptureButton.style.display = "block";
+    recordImg.style.display = "block";
+
+    recorder = RecordRTC(stream, {
+        type: 'gif',
+        frameRate: 1,
+        quality: 10,
+        width: 360,
+        hidden: 240,
+        onGifRecordingStarted: function() {
+            console.log('started')
+        },
+    });
+
+    // recorder.on("startRecord", function() {
+
+    //     console.log("Grabación iniciada!")
+
+    // });
+
+    // recorder.on("finishRecord", function() {
+
+    //     let videoBlob = recorder.recordedData.video;
+    //     console.log("Grabación terminada: ", videoBlob);
+
+    // });
+}
+
+function finishRecording() {
+
+    let finishCaptureButton = document.querySelector(".finish_capture_button");
+    let recordImg = document.getElementById("recordImg");
+    let repeatButton = document.querySelector(".repeat_button");
+    let uploadButton = document.querySelector(".upload_button");
+
+    finishCaptureButton.style.display = "none";
+    recordImg.style.display = "none";
+    repeatButton.style.display = "block";
+    uploadButton.style.display = "block";
+
+}
