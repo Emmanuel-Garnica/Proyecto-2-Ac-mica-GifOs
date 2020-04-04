63ab4b18e0bb98752898f85165feb8f10782bdf0
Emmanuel-Garnica <56373024+Emmanuel-Garnica@users.noreply.github.com>
2020-04-03 23:19:23 -0500

    GRABANDO GIF - Se logra iniciar  y detener la grabación generando un objeto con los datos obtenidos de la misma. (Funciona perfectamente en Mozzilla Firefox pero no en Chrome)

--- assets/scripts/subir_gifos.js
+++ assets/scripts/subir_gifos.js
@@ -1,9 +1,10 @@
-function startButton() {
+function allowRecord() {
+
     document.querySelector(".recordGif").style.display = "block";
     document.querySelector(".createGif").style.display = "none";
-}
 
-function allowRecord() {
+    // Pide permisos al usuario
+
     navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {
@@ -14,74 +15,83 @@ function allowRecord() {
 
     .then(function(recordGif) {
 
+            // Conecta lo que captura la cámara con el contenedor del video
+
             let video_Container = document.querySelector("video");
 
             video_Container.srcObject = recordGif;
             video_Container.play();
+
+            let captureButton = document.querySelector(".capture_button");
+            let cameraImg = document.getElementById("cameraImg");
+            let finishCaptureButton = document.querySelector(".finish_capture_button");
+            let recordImg = document.getElementById("recordImg");
+
+            // Se crea objeto recorder desde la libreria
+
+            recorder = RecordRTC(recordGif, {
+                type: 'gif',
+                frameRate: 1,
+                quality: 10,
+                width: 360,
+                hidden: 240,
+                onGifRecordingStarted: function() {
+                    console.log('started')
+                },
+            });
+
+            captureButton.addEventListener("click", () => {
+
+                recorder.startRecording();
+                console.log("Grabación iniciada!");
+
+                captureButton.style.display = "none";
+                cameraImg.style.display = "none";
+                finishCaptureButton.style.display = "block";
+                recordImg.style.display = "block";
+
+            })
+
+            finishCaptureButton.addEventListener("click", () => {
+
+                recorder.stopRecording();
+                console.log("Grabación Finalizada!");
+
+                let form = new FormData();
+
+                form.append("archivo", recorder.getBlob(), "Mi gif 1");
+                console.log(form.get("archivo"));
+
+                // declarar variable para URL: URL.createObjectURL +(form.get("archivo"))(formget en guias), seleccionar objeto de imagen y cambiar el src 
+
+                // Oculta botón de stop y muestra siguientes
+
+                let finishCaptureButton = document.querySelector(".finish_capture_button");
+                let recordImg = document.getElementById("recordImg");
+                let repeatButton = document.querySelector(".repeat_button");
+                let uploadButton = document.querySelector(".upload_button");
+
+                finishCaptureButton.style.display = "none";
+                recordImg.style.display = "none";
+                repeatButton.style.display = "block";
+                uploadButton.style.display = "block";
+
+            })
+
         })
         .catch(function(error) {
 
             alert("Es necesario permitir el uso de la cámara para continuar - " + error)
 
         })
-}
-
-function record() {
-    startButton();
-    allowRecord();
-}
-
-function startRecording() {
-
-    let captureButton = document.querySelector(".capture_button");
-    let cameraImg = document.getElementById("cameraImg");
-    let finishCaptureButton = document.querySelector(".finish_capture_button");
-    let recordImg = document.getElementById("recordImg");
-
-    captureButton.style.display = "none";
-    cameraImg.style.display = "none";
-    finishCaptureButton.style.display = "block";
-    recordImg.style.display = "block";
-
-    let mediaStream = document.querySelector(".myGifOs_box");
-
-    let recorder = new MediaStreamRecorder(mediaStream, {
-        type: 'gif',
-        frameRate: 1,
-        quality: 10,
-        width: 360,
-        hidden: 240,
-        onGifRecordingStarted: function() {
-            console.log('started')
-        },
-    });
-
-    recorder.startRecording();
-    console.log("Grabación iniciada!");
-    // recorder.on("startRecord", function() {
-
-    //     console.log("Grabación iniciada!")
-
-    // });
-
-    // recorder.on("finishRecord", function() {
-
-    //     let videoBlob = recorder.recordedData.video;
-    //     console.log("Grabación terminada: ", videoBlob);
-
-    // });
-}
-
-function finishRecording() {
-
-    let finishCaptureButton = document.querySelector(".finish_capture_button");
-    let recordImg = document.getElementById("recordImg");
-    let repeatButton = document.querySelector(".repeat_button");
-    let uploadButton = document.querySelector(".upload_button");
-
-    finishCaptureButton.style.display = "none";
-    recordImg.style.display = "none";
-    repeatButton.style.display = "block";
-    uploadButton.style.display = "block";
 
 }
+
+
+// function record() {
+
+//     // Muestra la siguiente ventana y activa la cámara en el contenedor del video
+
+//     startButton();
+//     allowRecord();
+// }
